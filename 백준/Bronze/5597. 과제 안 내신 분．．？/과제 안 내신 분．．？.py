8799f41f1b23daa402c49fbe6f199cd06083d709
김채민 <93961956+Aleph-Kim@users.noreply.github.com>
2023-11-20 15:06:44 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,9 +1,7 @@
-arr = [*range(31)]
-del (arr[0])
+arr = [*range(1, 31)]
 
 for _ in range(28):
-    del (arr[arr.index(int(input()))])
+    arr.remove(int(input()))
 
-arr.sort()
-print(arr[0])
-print(arr[1])
+for n in arr:
+    print(n)
