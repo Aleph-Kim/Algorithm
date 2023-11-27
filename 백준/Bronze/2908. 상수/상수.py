a04dcf8f1a0951ab8c356df1a88cfd5afad179a7
김채민 <93961956+Aleph-Kim@users.noreply.github.com>
2023-11-27 16:09:02 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,2 +1,1 @@
-a, b = list(map(list, map(reversed, map(list, input().split()))))
-print(int(''.join(a)) if int(''.join(a)) > int(''.join(b)) else int(''.join(b)))
+print(max(input()[::-1].split()))
