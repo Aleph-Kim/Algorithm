38a4c0248942ee6183806659a711bea308136be1
김채민 <93961956+Aleph-Kim@users.noreply.github.com>
2023-12-07 11:18:11 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,5 +1,5 @@
-while (True):
+while (1):
     x = input()
     if x == '0':
         break
-    print('yes') if str(''.join(list(reversed(x)))) == x else print('no')
+    print('yes') if x[::-1] == x else print('no')
