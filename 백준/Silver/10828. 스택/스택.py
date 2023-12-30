33b1893e5d0f5ebc0e2942b0a1cb27958ec87f9b
김채민 <93961956+Aleph-Kim@users.noreply.github.com>
2023-12-30 22:27:06 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+arr = []
+
+for _ in range(int(input())):
+    arr2 = input().split()
+
+    match arr2[0]:
+        case 'push':
+            arr.append(arr2[1])
+        case 'pop':
+            print(arr.pop() if arr else -1)
+        case 'size':
+            print(len(arr))
+        case 'empty':
+            print(0 if arr else 1)
+        case 'top':
+            print(arr[len(arr) - 1] if arr else -1)
