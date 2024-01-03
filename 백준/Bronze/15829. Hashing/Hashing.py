3204120265d5cce109393840ce17fda2917c7515
김채민 <93961956+Aleph-Kim@users.noreply.github.com>
2024-01-03 17:59:25 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,8 @@
+input()
+
+arr = list('abcdefghijklmnopqrstuvwxyz')
+str_arr = list(input())
+total = 0
+for i in range(len(str_arr)):
+    total += (arr.index(str_arr[i]) + 1) * (31 ** i)
+print(total)
