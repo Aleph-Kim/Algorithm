bbdd171079628ab767de36317ca644e894371f5d
김채민 <dktjdej25@gmail.com>
2024-02-20 14:52:01 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,4 +1,4 @@
-def decCheck(i):
+def pnCheck(i):
     for j in range(2, int(i ** 0.5)+1):
         if i == j*j or i % j == 0:
             return False
@@ -12,14 +12,14 @@ def solution(nums):
         for j in range(i+1, num_len-1):
             for k in range(j+1, num_len):
                 sums.append(sum([nums[i], nums[j], nums[k]]))
-    dec = []
+    pn = []
     
     for i in sums:
-        if i in dec:
-            dec.append(i)
+        if i in pn:
+            pn.append(i)
             continue
             
-        if decCheck(i):
-            dec.append(i)
+        if pnCheck(i):
+            pn.append(i)
 
-    return len(dec)
+    return len(pn)
